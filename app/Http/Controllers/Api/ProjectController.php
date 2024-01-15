809ac8b5528f862193c2e12de974f6bd1329baac
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2024-01-15 18:32:28 +0100

    CHANGE - project in results

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
 
     return response()->json([
         'success' => true,
-        'projects' => $projects
+        'results' => $projects
     ]);
    }
 }
